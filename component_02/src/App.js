f982a58a5aea0fcfcbbc684ad00b56789a2be8a1
quyenhair2109 <quyenphan2109@gmail.com>
2020-08-24 22:11:05 +0700

    bài 2 components

---
+++ component_02/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import 'bootstrap/dist/css/bootstrap.css';
+
+function App() {
+  return (
+    <div>
+      <h1>App Component</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ component_02/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import 'bootstrap/dist/css/bootstrap.css';
+
+function Header() {
+  return (
+    <div>
+      <h1>App Component</h1>
+    </div>
+  );
+}
+
+export default Header;
